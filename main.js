57c1ea978b41742eb53c159f5d58be11a331fc4c
jacobfield <jacobprogrammes@gmail.com>
2023-09-19 11:54:56 +0100

    kata 1

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 // code wars
 
+// Kata 1
+
+// Function to return the result of a*b
+// Set a variable as the result of a*b
+// Return the result of a*b, stored in the variable 'result
+// Call the function and pass in two arguments
+
+function multiply(a, b){
+    let result = a * b;
+     return result;
+   }
+   
+   multiply(5, 10);
+   
